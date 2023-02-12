bb9d34cfd91fe66efeb76b48952f5ed482866343
Jerry Hogsett <jhogsett@gmail.com>
2023-02-11 18:37:46 -0800

    add the ability to handle frame resynthesis

--- interpolate_series.py
+++ interpolate_series.py
@@ -12,7 +12,8 @@ def main():
     parser.add_argument("--model", default="./pretrained_models/pretrained_VFIformer/net_220.pth", type=str)
     parser.add_argument("--gpu_ids", type=str, default="0", help="gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU")
     parser.add_argument("--input_path", default="./images", type=str, help="Input path for PNGs to interpolate")
-    parser.add_argument("--depth", default=2, type=int, help="how many doublings of the frames")
+    parser.add_argument("--depth", default=2, type=int, help="How many doublings of the frames")
+    parser.add_argument("--offset", default=1, type=int, help="Frame series offset, 1 for slow motion, 2+ for frame resynthesis")
     parser.add_argument("--output_path", default="./output", type=str, help="Output path for interpolated PNGs")
     parser.add_argument("--base_filename", default="interpolated_frames", type=str, help="Base filename for interpolated PNGs")
     parser.add_argument("--verbose", dest="verbose", default=False, action="store_true", help="Show extra details")
@@ -26,7 +27,7 @@ def main():
     series_interpolater = InterpolateSeries(deep_interpolater, log.log)
 
     file_list = get_files(args.input_path, extension="png")
-    series_interpolater.interpolate_series(file_list, args.output_path, args.depth, args.base_filename)
+    series_interpolater.interpolate_series(file_list, args.output_path, args.depth, args.base_filename, args.offset)
 
 class InterpolateSeries():
     def __init__(self,
@@ -39,23 +40,32 @@ class InterpolateSeries():
         if self.log_fn:
             self.log_fn(message)
 
-    def interpolate_series(self, file_list : list, output_path : str, num_splits : int, base_filename : str):
+    def interpolate_series(self, file_list : list, output_path : str, num_splits : int, base_filename : str, offset : int = 1):
         file_list = sorted(file_list)
         count = len(file_list)
         num_width = len(str(count))
-
         pbar_desc = "Frames" if num_splits < 2 else "Total"
-        # stop short of beginning at the last frame since there's no frame after i
-        for n in tqdm(range(count-1), desc=pbar_desc, position=0):
+
+        for n in tqdm(range(count - offset), desc=pbar_desc, position=0):
             # for other than the first around, the duplicated real "before" frame is deleted for
             # continuity, since it's identical to the "after" from the previous round
             continued = n > 0
+
+            # if the offset is > 1 treat this as a resynthesis of frames
+            # and inform the deep interpolator to not keep the real frames
+            resynthesis = offset > 1
+
             before_file = file_list[n]
-            after_file = file_list[n+1]
-            filename = base_filename + "[" + str(n).zfill(num_width) + "]"
+            after_file = file_list[n + offset]
+
+            # if a resynthesis, start the file numbering at 1 to match the restored frame
+            # if an offset other than 2 is used, the frame numbers won't generally match
+            base_index = n + (1 if resynthesis else 0)
+            filename = base_filename + "[" + str(base_index).zfill(num_width) + "]"
+
             inner_bar_desc = f"Frame #{n}"
-            self.log("creating inflated frames files starting with " + filename)
-            self.deep_interpolater.split_frames(before_file, after_file, num_splits, output_path, filename, progress_label=inner_bar_desc, continued=continued)
+            self.log(f"creating inflated frames for frame files {before_file} - {after_file}")
+            self.deep_interpolater.split_frames(before_file, after_file, num_splits, output_path, filename, progress_label=inner_bar_desc, continued=continued, resynthesis=resynthesis)
 
 if __name__ == '__main__':
     main()
