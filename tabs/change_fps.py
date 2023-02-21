24e88d8a4d1c700bb058c2dbaf9f9c6a341c2e85
Jerry Hogsett <jhogsett@gmail.com>
2023-02-20 17:17:02 -0800

    wip on tabs

--- tabs/change_fps.py
+++ tabs/change_fps.py
@@ -12,9 +12,10 @@ from interpolate_engine import InterpolateEngine
 from interpolate import Interpolate
 from interpolation_target import TargetInterpolate
 from resample_series import ResampleSeries
-from resequence_files import ResequenceFiles
+from resequence_files import ResequenceFiles as _ResequenceFiles
 
 class ChangeFPS():
+    """Encapsulates UI elements and events for the Change FPS feature"""
     def __init__(self,
                     config : SimpleConfig,
                     engine : InterpolateEngine,
@@ -24,44 +25,70 @@ class ChangeFPS():
         self.log_fn = log_fn
 
     def log(self, message : str):
+        """Logging"""
         self.log_fn(message)
 
     def render_tab(self):
+        """Render tab into UI"""
         max_fps = self.config.fps_change_settings["maximum_fps"]
         starting_fps = self.config.fps_change_settings["starting_fps"]
         ending_fps = self.config.fps_change_settings["ending_fps"]
         max_precision = self.config.fps_change_settings["max_precision"]
         precision = self.config.fps_change_settings["default_precision"]
-        lowest_common_rate, filled, sampled, fractions, predictions = fps_change_details(starting_fps, ending_fps, precision)
-        e = {}
+        lowest_common_rate, filled, sampled, fractions, predictions\
+            = fps_change_details(starting_fps, ending_fps, precision)
         with gr.Tab(SimpleIcons.FILM + "Change FPS"):
-            gr.HTML("Change the frame rate for a set of PNG video frames using frame search", elem_id="tabheading")
+            gr.HTML("Change the frame rate for a set of PNG video frames using frame search",
+                elem_id="tabheading")
             with gr.Row():
                 with gr.Column():
                     with gr.Row():
-                        e["input_path_text_fc"] = gr.Text(max_lines=1, label="Input Path", placeholder="Path on this server to the PNG frame files to be converted")
-                        e["output_path_text_fc"] = gr.Text(max_lines=1, label="Output Path", placeholder="Path on this server for the converted PNG frame files, leave blank to use default")
+                        input_path_text_fc = gr.Text(max_lines=1, label="Input Path",
+                        placeholder="Path on this server to the PNG frame files to be converted")
+                        output_path_text_fc = gr.Text(max_lines=1, label="Output Path",
+placeholder="Path on this server for the converted PNG frame files, leave blank to use default")
                     with gr.Row():
-                        e["starting_fps_fc"] = gr.Slider(value=starting_fps, minimum=1, maximum=max_fps, step=1, label="Starting FPS")
-                        e["ending_fps_fc"] = gr.Slider(value=ending_fps, minimum=1, maximum=max_fps, step=1, label="Ending FPS")
-                        e["output_lcm_text_fc"] = gr.Text(value=lowest_common_rate, max_lines=1, label="Lowest Common FPS", interactive=False)
-                        e["output_filler_text_fc"] = gr.Text(value=filled, max_lines=1, label="Filled Frames per Input Frame", interactive=False)
-                        e["output_sampled_text_fc"] = gr.Text(value=sampled, max_lines=1, label="Output Frames Sample Rate", interactive=False)
+                        starting_fps_fc = gr.Slider(value=starting_fps, minimum=1, maximum=max_fps,
+                            step=1, label="Starting FPS")
+                        ending_fps_fc = gr.Slider(value=ending_fps, minimum=1, maximum=max_fps,
+                            step=1, label="Ending FPS")
+                        output_lcm_text_fc = gr.Text(value=lowest_common_rate, max_lines=1,
+                            label="Lowest Common FPS", interactive=False)
+                        output_filler_text_fc = gr.Text(value=filled, max_lines=1,
+                            label="Filled Frames per Input Frame", interactive=False)
+                        output_sampled_text_fc = gr.Text(value=sampled, max_lines=1,
+                            label="Output Frames Sample Rate", interactive=False)
                     with gr.Row():
-                        e["precision_fc"] = gr.Slider(value=precision, minimum=1, maximum=max_precision, step=1, label="Precision")
-                        e["times_output_fc"] = gr.Textbox(value=fractions, label="Frame Search Times", max_lines=8, interactive=False)
-                        e["predictions_output_fc"] = gr.Textbox(value=predictions, label="Predicted Matches", max_lines=8, interactive=False)
+                        precision_fc = gr.Slider(value=precision, minimum=1, maximum=max_precision,
+                            step=1, label="Precision")
+                        times_output_fc = gr.Textbox(value=fractions, label="Frame Search Times",
+                            max_lines=8, interactive=False)
+                        predictions_output_fc = gr.Textbox(value=predictions,
+                            label="Predicted Matches", max_lines=8, interactive=False)
             gr.Markdown("*Progress can be tracked in the console*")
-            e["convert_button_fc"] = gr.Button("Convert " + SimpleIcons.SLOW_SYMBOL, variant="primary")
+            convert_button_fc = gr.Button("Convert " + SimpleIcons.SLOW_SYMBOL, variant="primary")
             with gr.Accordion(SimpleIcons.TIPS_SYMBOL + " Guide", open=False):
                 WebuiTips.change_fps.render()
-        e["starting_fps_fc"].change(update_info_fc, inputs=[e["starting_fps_fc"], e["ending_fps_fc"], e["precision_fc"]], outputs=[e["output_lcm_text_fc"], e["output_filler_text_fc"], e["output_sampled_text_fc"], e["times_output_fc"], e["predictions_output_fc"]], show_progress=False)
-        e["ending_fps_fc"].change(update_info_fc, inputs=[e["starting_fps_fc"], e["ending_fps_fc"], e["precision_fc"]], outputs=[e["output_lcm_text_fc"], e["output_filler_text_fc"], e["output_sampled_text_fc"], e["times_output_fc"], e["predictions_output_fc"]], show_progress=False)
-        e["precision_fc"].change(update_info_fc, inputs=[e["starting_fps_fc"], e["ending_fps_fc"], e["precision_fc"]], outputs=[e["output_lcm_text_fc"], e["output_filler_text_fc"], e["output_sampled_text_fc"], e["times_output_fc"], e["predictions_output_fc"]], show_progress=False)
-        e["convert_button_fc"].click(self.convert_fc, inputs=[e["input_path_text_fc"], e["output_path_text_fc"], e["starting_fps_fc"], e["ending_fps_fc"], e["precision_fc"]])
-        return e
+        starting_fps_fc.change(update_info_fc,
+            inputs=[starting_fps_fc, ending_fps_fc, precision_fc],
+            outputs=[output_lcm_text_fc, output_filler_text_fc, output_sampled_text_fc,
+                times_output_fc, predictions_output_fc], show_progress=False)
+        ending_fps_fc.change(update_info_fc, inputs=[starting_fps_fc, ending_fps_fc, precision_fc],
+            outputs=[output_lcm_text_fc, output_filler_text_fc, output_sampled_text_fc,
+                times_output_fc, predictions_output_fc], show_progress=False)
+        precision_fc.change(update_info_fc, inputs=[starting_fps_fc, ending_fps_fc, precision_fc],
+            outputs=[output_lcm_text_fc, output_filler_text_fc, output_sampled_text_fc,
+                times_output_fc, predictions_output_fc], show_progress=False)
+        convert_button_fc.click(self.convert_fc, inputs=[input_path_text_fc, output_path_text_fc,
+            starting_fps_fc, ending_fps_fc, precision_fc])
 
-    def convert_fc(self, input_path : str, output_path : str, starting_fps : int, ending_fps : int, precision : int):
+    def convert_fc(self,
+                    input_path : str,
+                    output_path : str,
+                    starting_fps : int,
+                    ending_fps : int,
+                    precision : int):
+        """Run the Change FPS process"""
         if input_path:
             interpolater = Interpolate(self.engine.model, self.log)
             target_interpolater = TargetInterpolate(interpolater, self.log)
@@ -70,8 +97,11 @@ class ChangeFPS():
                 base_output_path = output_path
                 create_directory(base_output_path)
             else:
-                base_output_path, _ = AutoIncrementDirectory(self.config.directories["output_fps_change"]).next_directory("run")
-            series_resampler.resample_series(input_path, base_output_path, starting_fps, ending_fps, precision, f"resampled@{starting_fps}")
+                base_output_path, _ = AutoIncrementDirectory(
+                    self.config.directories["output_fps_change"]).next_directory("run")
+            series_resampler.resample_series(input_path, base_output_path, starting_fps,
+                ending_fps, precision, f"resampled@{starting_fps}")
 
             self.log(f"auto-resequencing sampled frames at {output_path}")
-            ResequenceFiles(base_output_path, "png", f"resampled@{ending_fps}fps", 0, 1, -1, True, self.log).resequence()
+            _ResequenceFiles(base_output_path, "png", f"resampled@{ending_fps}fps", 0, 1, -1, True,
+                 self.log).resequence()
